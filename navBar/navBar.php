12793c90aa9d2867591f7e7d1eb950b52f4d7f92
Kennedy12 <kennedy12@localhost.localdomain>
2018-06-28 14:32:56 +0200

    Modificati navbar.{php,css} migliorati bottoni, corretti alcuni errori

--- navBar/navBar.php
+++ navBar/navBar.php
@@ -3,18 +3,18 @@
 -->
 
 <nav class="navbar navbar-expand-md d-flex flex-column flex-md-row justify-content-md-between  mb-3 bg-dark navbar-dark navbar-shrink fixed-top" id="navBar">  
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbarTogglerSignupLogin" aria-controls="navbarTogglerSignupLogin" aria-expanded="false" aria-label="Toggle navigation">
         <span>
-            <img class"navbar-toggler-icon" src="img/Time1.png" style="width:40px;">
+            <img class="sizeTitle" src="img/Time1.png">
                 ShareYourTime
         </span>
     </button>
 
     <div>
-        <div class="collapse navbar-collapse navbarTogglerDemo01">
+        <div class="collapse navbar-collapse navbarTogglerSignupLogin">
             <ul class="navbar-nav">
             <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
             </li>
             </ul>
         </div>
@@ -22,17 +22,17 @@
     
     <div class="d-none d-md-inline">
         <a class="navbar-brand mt-2 mt-lg-0" href="#">
-            <img src="img/Time1.png" style="width:40px;">
+            <img src="img/Time1.png" class="sizeTitle">
             ShareYourTime
         </a>
     </div>
     <div>
-        <span class="collapse navbar-collapse navbarTogglerDemo01">	
-            <button type="button" href="#" class="btn btn-primary mr-2 d-block d-sm-inline"  data-toggle="modal" data-target="#signUpModalTarget">
+        <span class="collapse navbar-collapse navbarTogglerSignupLogin">	
+            <button type="button" href="#" class="btn btn-primary mr-2 d-block d-sm-inline btnSize"  data-toggle="modal" data-target="#signUpModalTarget">
                 <i class="fa fa-user-plus"></i>
                     Sign-Up
             </button>
-            <button type="button" class="btn btn-primary mt-2 mt-sm-0 ml-2" data-toggle="modal" data-target="#loginModalTarget">
+            <button type="button" class="btn btn-success mt-2 mt-sm-0 btnSize" data-toggle="modal" data-target="#loginModalTarget">
             <i class="fas fa-sign-in-alt"></i>
                     Login
             </button>
