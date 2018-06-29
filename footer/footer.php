66732809435a613bd6b756c94135d137eec618f9
Kennedy12 <kennedy12@localhost.localdomain>
2018-06-29 17:13:16 +0200

    Aggiunto footer, ma non e' responsive

--- footer/footer.php
+++ footer/footer.php
@@ -1,16 +1,47 @@
-<!--
-    footer :
-        -abbozzo di suddivisione del footer
---> 
-<footer class="footer d-sm-flex justify-content-between bg-dark">
-    <div>
-        div 1
-    </div>
-    <div >
-        div 2 
-    </div>
-    <div >
-        div 3
-    </div>
-
+<footer class="footer bg-dark">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-3 d-block mt-3">
+				<p class="text-center myFont">Contatti</p>
+				<p class="font-italic myFont">Email: example@test.com</p>
+				<p class="font-italic myFont">Telefono: 555-1234-4321</p>
+			</div>
+			<div class="col-sm-6 mt-3">
+				<p class="myFont bigFont">SHAREYOURTIME</p>
+				<img class="logoDim" src="img/sandclock.png">
+				<br>
+				<p class="myFont">Copyright &copy; Brandi Eugenio &amp; Canepa Andrea</p>
+			</div>
+			<div class="col-mx-3 mt-3">
+				<p class="font-italic myFont">Social</p>
+				<ul class="list-inlinei center">
+					<li class="list-inline-item">
+						<a href="www.facebook.com">
+							<img class="socialIconDim" src="img/fb_logo.png"></i>
+						</a>
+					</li>
+					<li class="list-inline-item">
+						<a href="www.github.com">
+							<img class="socialIconDim" src="img/git_logo.png"></i>
+						</a>
+					</li>
+					<li class="list-inline-item">
+						<a href="www.instagram.com">
+							<img class="socialIconDim" src="img/insta_logo.png"></i>
+						</a>
+					</li>
+					<li class="list-inline-item">
+						<a href="www.github.com">
+							<img class="socialIconDim" src="img/twitter_logo.png"></i>
+						</a>
+					</li>
+					<li class="list-inline-item">
+						<a href="www.youtube.com">
+							<img class="socialIconDim" src="img/yt_logo.png"></i>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
 </footer>
