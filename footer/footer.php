cf011156bfccb28d54d2cd4d4cccd63106958cfb
Kennedy12 <kennedy12@localhost.localdomain>
2018-06-30 14:05:23 +0200

    Footer ora responsive e centrato correttamente

--- footer/footer.php
+++ footer/footer.php
@@ -1,42 +1,42 @@
-<footer class="footer bg-dark">
+<footer class="footer bg-dark footerCenter">
 	<div class="container">
 		<div class="row">
-			<div class="col-sm-3 d-block mt-3">
+			<div class="col-lg-3 mt-3">
 				<p class="text-center myFont">Contatti</p>
 				<p class="font-italic myFont">Email: example@test.com</p>
 				<p class="font-italic myFont">Telefono: 555-1234-4321</p>
 			</div>
-			<div class="col-sm-6 mt-3">
+			<div class="col-lg-5 mt-3">
 				<p class="myFont bigFont">SHAREYOURTIME</p>
 				<img class="logoDim" src="img/sandclock.png">
 				<br>
 				<p class="myFont">Copyright &copy; Brandi Eugenio &amp; Canepa Andrea</p>
 			</div>
-			<div class="col-mx-3 mt-3">
+			<div class="col-lg-4 mt-3">
 				<p class="font-italic myFont">Social</p>
-				<ul class="list-inlinei center">
+				<ul class="list-inline">
 					<li class="list-inline-item">
-						<a href="www.facebook.com">
+						<a href="https://www.facebook.com">
 							<img class="socialIconDim" src="img/fb_logo.png"></i>
 						</a>
 					</li>
 					<li class="list-inline-item">
-						<a href="www.github.com">
+						<a href="https://www.github.com">
 							<img class="socialIconDim" src="img/git_logo.png"></i>
 						</a>
 					</li>
 					<li class="list-inline-item">
-						<a href="www.instagram.com">
+						<a href="https://www.instagram.com">
 							<img class="socialIconDim" src="img/insta_logo.png"></i>
 						</a>
 					</li>
 					<li class="list-inline-item">
-						<a href="www.github.com">
+						<a href="https://www.github.com">
 							<img class="socialIconDim" src="img/twitter_logo.png"></i>
 						</a>
 					</li>
 					<li class="list-inline-item">
-						<a href="www.youtube.com">
+						<a href="https://www.youtube.com">
 							<img class="socialIconDim" src="img/yt_logo.png"></i>
 						</a>
 					</li>
--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     ?>
     <link rel="stylesheet" type="text/css" href="index.css"/>
     <link rel="stylesheet" type="text/css" href="navBar/navBar.css"/> 
-   
+	<link rel="stylesheet" type="text/css" href="footer/footer.css"/>
   </head>
 
 	<body>
@@ -112,7 +112,7 @@
         require ('footer/footer.php');
      ?>
 
-    <script type="text/javascript" src="index.js"></script>
+    <!--script type="text/javascript" src="index.js"></script-->
 <!---->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
