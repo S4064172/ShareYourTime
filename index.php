9e38a0ec5d2e803ca8489a5d2d9d305c104c539d
Kennedy12 <kennedy12@localhost.localdomain>
2018-07-02 15:38:51 +0200

    Aggiunto abbozza di tabella e jumbotron nell'index e font nella pagina.

--- index.php
+++ index.php
@@ -1,36 +1,61 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <?php
-      require ('header/header.html');
-    ?>
-    <link rel="stylesheet" type="text/css" href="index.css"/>
-    <link rel="stylesheet" type="text/css" href="navBar/navBar.css"/> 
-	<link rel="stylesheet" type="text/css" href="footer/footer.css"/>
-  </head>
-
+ 	<head>
+    	<?php
+	      require ('header/header.html');
+	    ?>
+    	<link rel="stylesheet" type="text/css" href="index.css"/>
+	    <link rel="stylesheet" type="text/css" href="navBar/navBar.css"/> 
+		<link rel="stylesheet" type="text/css" href="footer/footer.css"/>
+	  </head>
+	
 	<body>
     <?php
         require ('navBar/navBar.php');
-     ?>
-        
-	<section  id="homeView" >
-      <div class="container">
-        <div class="row">
-          <!--
-          -> descrizine generale sito
-          -->
-          <div class="col-md-6 offset-lg-6 offset-md-4 offset-sm-2" id="aboutUs">
+	?>
 
-          </div> 
-        </div>
-      </div>  
+	
+	<section id="homeView">
+		<div class="container">
+    		<div class="row">
+	        <!--
+    	    	 descrizine generale sito
+        	-->
+				<div class="col-md-6 offset-md-6 offset-md-4 offset-md-2 mt-5" >
+					<div class="container">
+						<div class="jumbotron infoJumbo">
+							<h1><b>Share Your Time</b></h1>
+							<br>
+							<p>
+								Hai un minuto da dedicare agli altri ? <br>
+								Hai esperienza in un particolare settore ? <br>
+								Cerchi di mettere qualche soldo da parte ? <br>
+								Sei la persona giusta per noi !
+							</p>
+						</div>
+					<div>
+				</div> 
+			</div>
+		</div>  
 
+
+		<div class="container">
+    		<div class="row">
+	        <!--
+    	    	 ultimi lavori caricati
+        	-->
+				<div class="col-md-6 offset-md-6 offset-md-4 offset-md-2 mt-5" >
+					<?php
+						require('last5.html');
+					?>
+				</div>
+			</div>
+		<div-->
 
       <!--
           -> Login a scomparsa
       -->	
-	<div class="modal myModal fade" id="loginModalTarget" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal myModal myModal-mt fade" id="loginModalTarget" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     	<div class="modal-dialog" role="document">
         	<div class="modal-content animate">
             	<form  action="login.php" method="POST">
@@ -40,10 +65,10 @@
 		              	</button>
 			            <img src="img/avatar.png" alt="Avatar" class="avatar">
 					</div> 
-					<div class="myContainer">
-	            		<label for="usernameLogin"><b>Username</b></label>
+					<div class="myContainer-padding">
+	            		<label for="usernameLogin" class="text-c"><b>Username</b></label>
 			            	<input type="text" placeholder="Enter Username" name="usernameLogin" minlength="5" maxlength="125" required>
-	    	    	    <label for="pswLogin"><b>Password</b></label>
+	    	    	    <label for="pswLogin" class="text-c"><b>Password</b></label>
     	    	    		<input type="password" placeholder="Enter Password" name="pswLogin" minlength="8" maxlength="125" required>
 	    	    	    <button type="submit" class="btn btn-success mybutton mt-4">Login</button>
 					</div>	
@@ -67,7 +92,7 @@
 						<label class="signupText">Benvenuto !</label>&nbsp;
 						<img src="img/Time1.png" alt="Avatar" class="avatar">
 					</div>
-					<div class="container myContainer">
+					<div class="container myContainer-padding">
 						<div class="row">
 							<div class="col-md-6">
 								<label for="usernameReg"><b>Username</b></label>
@@ -86,13 +111,13 @@
 									<input type="text" placeholder="Inserisci il tuo cognome" name="surnameReg" maxlength="125" required>
 								<label for="emailReg" class="text-r"><b>Email</b></label>
 									<input type="email" placeholder="Inserisci il tuo indirizzo email" name="emailReg" maxlength="125" required>
-								<label for="telephone" class="text-r"><b>Telefono(opzionale)</b></label>
-									<input type="tel" placeholder="Inserisci un numero di telefono" name="telephone" minlength="10" maxlength="10">
+								<label for="telephone" class="text-r"><b>Telefono</b></label>
+									<input type="tel" placeholder="Inserisci un numero di telefono" name="telephone" minlength="10" maxlength="10" required>
 							</div>
 						</div>
 						<label for="profilePhoto">Immagine del profilo</label>&nbsp;&nbsp;&nbsp;&nbsp;
 							<input type="file" name="profilePhoto">
-						<button type="submit" class="btn btn-success mybutton mt-4">Register</button>
+						<button type="submit" class="btn btn-success mybutton mt-4">Registrati</button>
 					</div>
 				</form>
 		    </div>
@@ -101,11 +126,10 @@
 	</section>
 
      <?php
-        require ('footer/footer.php');
+        require('footer/footer.php');
      ?>
 
     <!--script type="text/javascript" src="index.js"></script-->
-<!---->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
