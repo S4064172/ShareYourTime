b0dab7203f6eb752aaba085b0df8d1b00115cfca
Eugenio <s4064172@studenti.unige.it>
2018-07-07 11:38:21 +0200

    -> checkLogin.php     -> aggiunti controlli campi     -> manca sesione e segnalazione di errore

--- modalRegistrazione-Login/checkLogin.php
+++ modalRegistrazione-Login/checkLogin.php
@@ -5,22 +5,46 @@
     require_once("../utils/regexConstant.php");
 
 
+    function checkIfExistInDb($fieldSearchUser,  $fieldSearchPsw )
+	{
+		$conn = selectionDB();
+
+        $fieldSearchUser = sanitizeToSql($fieldSearchUser, $conn);
+        $fieldSearchPsw = sanitizeToSql($fieldSearchPsw, $conn);
+        $fieldSearchPsw=sha1($fieldSearchPsw);
+		$querySelectUser = 	"SELECT user ".
+							"FROM ShareYourUsersTime ".
+							"WHERE user=? and password=?";
+
+		if ( ($prep_stmt = mysqli_prepare($conn, $querySelectUser)) ) {
+			if ( !mysqli_stmt_bind_param($prep_stmt, "ss", $fieldSearchUser, $fieldSearchPsw))
+				die ("Errore nell'accoppiamento dei parametri<br>");
+			
+			if ( !mysqli_stmt_execute($prep_stmt) )
+				die ("Errore nell'esecuzione della query<br>");
+
+			mysqli_stmt_store_result($prep_stmt);
+			$row = mysqli_stmt_num_rows($prep_stmt);
+			
+			mysqli_stmt_close($prep_stmt);
+            mysqli_close($conn);
+			return ($row == 1);
+		}
+		die ("Errore nella preparazione della query<br>");
+	}
+
 
     foreach ($_POST as $key => $value) 
-            checkDataNullOrEmpty($key);
+            check_POST_NotIsSetOrEmpty($key);
             
-    if (!checkMatchRegex($_Post['usernameLogin'],alphaNumRegex))
-        die("errore");
+    if ( !checkMatchRegex($_POST['usernameLogin'], alphaNumRegex) )
+        die("errore0");
 
     foreach(passwordRegex as $regex) {
-        if ( !checkMatchRegex($_POST['pswReg'], $regex) ) 
-            die("errore");
+        if ( !checkMatchRegex($_POST['pswLogin'], $regex) ) 
+            die("errore1");
         
     }
-
-    $conn = selectionDB();
-
-    $loginSanitize = sanitizeToSql($_Post['usernameLogin'],$conn);
-
-    
+    if(!checkIfExistInDb($_POST['usernameLogin'],$_POST['pswLogin']))
+        die("errore2");
 
