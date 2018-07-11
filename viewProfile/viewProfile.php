93755a32e62a163db05872f169cae5c6e78691ff
Eugenio <s4064172@studenti.unige.it>
2018-07-11 22:49:38 +0200

    -> homepage.css     -> piccola modifica alla grafica
    -> viewProfile.css
        -> agginto caricamento informazioni user dal bd

--- viewProfile/viewProfile.php
+++ viewProfile/viewProfile.php
@@ -1,9 +1,19 @@
-<?php require_once('utils/dataBaseConstant.php');?>
+<?php 
+    require_once('utils/dataBaseConstant.php');
+    require_once('db/connection.php');
 
-<div class="myContainerMarginTop">
+    $conn = connectionToDb();
+    $getInfoUserQuery = "SELECT User,Name,Surname,Phone,Email,Street FROM ShareYourUsersTime where user='".$_SESSION['user']."'";
+    if ( !($res = mysqli_query($conn, $getInfoUserQuery)) ) 
+                die('Errore nella selezione dei lavori');
+    $row = mysqli_fetch_array($res);
+    mysqli_free_result($res);
+?>
 
-    <div class="row">
-        <div class="offset-4 col-md-6">
+<div class="myContainerPageToView">
+
+    <div class="row text-center">
+        <div class="col-12">
             <h1>Impostazioni Profilo</h1>
         </div>  
     </div>
@@ -19,7 +29,7 @@
                         <label class="labelText"><b>Username</b></label>
                     </div>
                     <div class="col-md-6">
-                        <input onfocusout="" onfocusin="cleanErr('')" id="userModified" type="text" placeholder="Inserisci un username" name="userModified" minlength=<?php echo UserNameMinLength?> maxlength=<?php echo UserNameMaxLength?> readonly>
+                        <input onfocusout="" onfocusin="cleanErr('')" id="userModified" type="text" value="<?php echo $row['User'] ?>" name="userModified" minlength=<?php echo UserNameMinLength?> maxlength=<?php echo UserNameMaxLength?> readonly>
                         <p id="errUserModified"></p> 
                     </div>
                 </div>
@@ -29,7 +39,7 @@
                     <label class="labelText"><b>Email</b></label>
                     </div>
                     <div class="col-md-6">
-                        <input onfocusout="" onfocusin="cleanErr('')" id="emailModified" type="email" placeholder="Inserisci il tuo indirizzo email" name="emailModified" minlength=<?php echo EmailMinLength?> maxlength=<?php echo EmailMaxLength?> readonly>
+                        <input onfocusout="" onfocusin="cleanErr('')" id="emailModified" type="email" value="<?php echo $row['Email'] ?>" name="emailModified" minlength=<?php echo EmailMinLength?> maxlength=<?php echo EmailMaxLength?> readonly>
                         <p id="errEmailModified"></p>
                     </div>
                 </div>
@@ -39,7 +49,7 @@
                         <label class="labelText fieldHide" id="pswLabel"><b>Password</b></label>
                     </div>
                     <div class="col-md-6">
-                        <input onfocusout="" onfocusin="cleanErr('')" id="pswModified" type="password" placeholder="Inserisci una password" name="pswModified" minlength=<?php echo PasswordMinLength?> readonly>
+                        <input onfocusout="" onfocusin="cleanErr('')" id="pswModified" type="password" name="pswModified" minlength="<?php echo PasswordMinLength?>" readonly>
                         <p id="errPswModified"></p>
                     </div>
                 </div>
@@ -49,7 +59,7 @@
                         <label class="labelText"><b>Nome</b></label>
                     </div>
                     <div class="col-md-6">
-                        <input onfocusout="" onfocusin="cleanErr('')" id="nameModified" type="text" placeholder="Inserisci il tuo nome" name="nameModified" minlength=<?php echo NameMinLength?> maxlength=<?php echo NameMaxLength?> readonly>
+                        <input onfocusout="" onfocusin="cleanErr('')" id="nameModified" type="text" value="<?php echo $row['Name'] ?>" name="nameModified" minlength=<?php echo NameMinLength?> maxlength=<?php echo NameMaxLength?> readonly>
                         <p id="errNameModified"></p>
                     </div>
                 </div>
@@ -59,7 +69,7 @@
                         <label class="labelText"><b>Cognome</b></label>
                     </div>
                     <div class="col-md-6">
-                        <input onfocusout="" onfocusin="cleanErr('')" id="surnameModified" type="text" placeholder="Inserisci il tuo cognome" name="surnameModified" minlength=<?php echo SurnameMinLength?> maxlength=<?php echo SurnameMaxLength?> readonly> 
+                        <input onfocusout="" onfocusin="cleanErr('')" id="surnameModified" type="text" value="<?php echo $row['Surname'] ?>" name="surnameModified" minlength=<?php echo SurnameMinLength?> maxlength=<?php echo SurnameMaxLength?> readonly> 
                         <p id="errSurnameModified"></p>
                     </div>
                 </div>
@@ -69,7 +79,7 @@
                         <label class="labelText" ><b>Indirizzo</b></label>
                     </div>
                     <div class="col-md-6">
-                        <input onfocusout="" onfocusin="cleanErr('')" id="addressModified" type="text" placeholder="Inserisci il tuo indirizzo" name="addressModified" minlength=<?php echo StreetMinLength?> maxlength=<?php echo StreetMaxLength?> readonly>
+                        <input onfocusout="" onfocusin="cleanErr('')" id="addressModified" type="text" value="<?php echo $row['Street'] ?>" name="addressModified" minlength=<?php echo StreetMinLength?> maxlength=<?php echo StreetMaxLength?> readonly>
                         <p id="errAddressModified"></p>
                     </div>
                 </div>
@@ -79,15 +89,15 @@
                         <label class="labelText"><b>Telefono</b></label>
                     </div>
                     <div class="col-md-6">
-                        <input onfocusout="" onfocusin="cleanErr('')" id="phoneModified" type="tel" placeholder="Inserisci un numero di telefono" name="phoneModified" minlength=<?php echo PhoneLength?> maxlength=<?php echo PhoneLength?> readonly>
+                        <input onfocusout="" onfocusin="cleanErr('')" id="phoneModified" type="tel" value="<?php echo $row['Phone'] ?>" name="phoneModified" minlength=<?php echo PhoneLength?> maxlength=<?php echo PhoneLength?> readonly>
                         <p id="errPhoneModified"></p>
                     </div>
                 </div>
 
-                <div class="row">
+                <div class="row text-center">
                     <div class="col-md-6">
-                        <button class="btn btn-primary" id="bntModify" onClick="hideItem('bntModify');showItem('bntSave');showItem('pswLabel');showItem('pswModified');removeReadOnly();">Modifica</button>
-                        <button class="btn btn-primary fieldHide" id="bntSave" onClick="hideItem('bntSave');showItem('bntModify');hideItem('pswLabel');hideItem('pswModified');addReadOnly();">Salva</button>
+                        <button class="btn btn-primary mb-2 mb-sm-0" id="bntModify" onClick="hideItem('bntModify');showItem('bntSave');showItem('pswLabel');showItem('pswModified');removeReadOnly();">Modifica</button>
+                        <button class="btn btn-primary fieldHide mb-2 mb-sm-0" id="bntSave" onClick="hideItem('bntSave');showItem('bntModify');hideItem('pswLabel');hideItem('pswModified');addReadOnly();">Salva</button>
                     </div>
                     <div class="col-md-6">
                        <button class="btn btn-success" onClick="hideItem('pswLabel');hideItem('pswModified');addReadOnly();showHome();" id="bntExit">Esci</button>
