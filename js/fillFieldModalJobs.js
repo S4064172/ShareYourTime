444ba9f9cd374053617c7b3480bf6b4c73747872
Eugenio <s4064172@studenti.unige.it>
2018-07-19 16:47:19 +0200

    -> risolti conflitti v2.0

--- js/fillFieldModalJobs.js
+++ js/fillFieldModalJobs.js
@@ -7,7 +7,6 @@ function fillModalFieldJobs(){
     document.getElementById('modalCost').value=
         document.getElementById('cardCost').getAttribute('value');
     
-<<<<<<< HEAD
     var startDate =  document.getElementById('cardTimeStart').getAttribute('value').split(" ");
     document.getElementById('modalDateStart').value=startDate[0];
     var startTime =startDate[1].split(":");
@@ -18,18 +17,6 @@ function fillModalFieldJobs(){
     document.getElementById('modalDateEnd').value= endDate[0];
     var endTime =endDate[1].split(":")
     document.getElementById('modalTimeEnd').value= endTime[0]+":"+endTime[1];
-=======
-    document.getElementById('modalDateStart').value=
-        document.getElementById('cardTimeStart').getAttribute('value').split(" ")[0];
-    document.getElementById('modalTimeStart').value=
-        document.getElementById('cardTimeStart').getAttribute('value').split(" ")[1];
-
-    var endDate =  document.getElementById('cardTimeEnd').getAttribute('value').split(" ");
-    document.getElementById('modalDateEnd').value= end[0];
-    var endTime =end[1].split(":")
-    document.getElementById('modalTimeEnd').value= 
-        document.getElementById('cardTimeEnd').getAttribute('value').split(" ")[1].split(":")[0]+":"+document.getElementById('cardTimeEnd').getAttribute('value').split(" ")[1].split(":")[1];
->>>>>>> 1c69e0bb514b87200361309ab8652859ae77c9ef
     document.getElementById('modalDistance').value=
         document.getElementById('cardDistance').getAttribute('value');
     document.getElementById('modalStreet').value=
--- modalView/modalJobsView.php
+++ modalView/modalJobsView.php
@@ -8,19 +8,13 @@
           <div class="row">
             <div class="col-12 text-center">
               <label><b>Descrizione</b></label>
-<<<<<<< HEAD
               <input onfocusout="checkDescription('modalDescription','errModalDescription');" onfocusin="cleanErr('errModalDescription');" id="modalDescription" type="textModal" name="description" minlength=<?php echo DescriptionMinLength?> maxlength=<?php echo DescriptionMaxLength?>>
               <p id="errModalDescription"></p>                                                            
-=======
-              <input onfocusout="" onfocusin="" id="modalDescription" type="textModal" name="description" minlength=<?php echo DescriptionMinLength?> maxlength=<?php echo DescriptionMaxLength?>>
-              <p id=""></p>                                                            
->>>>>>> 1c69e0bb514b87200361309ab8652859ae77c9ef
             </div>
           </div>
           <div class="row">  
             <div class="col-md-6 text-center">
               <label ><b>Costo</b></label>
-<<<<<<< HEAD
               <input onfocusout="checkCost('modalCost','errModalCost')" onfocusin="cleanErr('errModalCost');" id="modalCost" type="textModal" name="cost">
               <p id="errModalCost"></p>
             </div>
@@ -28,15 +22,6 @@
               <label><b>Distanza</b></label>
               <input onfocusout="checkDistance('modalDistance','errModalDistance');" onfocusin="cleanErr('errModalDistance')" id="modalDistance" type="textModal" name="distance">
               <p id="errModalDistance"></p>
-=======
-              <input onfocusout="" onfocusin="" id="modalCost" type="textModal" name="cost">
-              <p id=""></p>
-            </div>
-            <div class="col-md-6 text-center">
-              <label><b>Distanza</b></label>
-              <input onfocusout="" onfocusin="" id="modalDistance" type="textModal" name="distance">
-              <p id=""></p>
->>>>>>> 1c69e0bb514b87200361309ab8652859ae77c9ef
             </div>
           </div>
           <div class="row">
@@ -62,13 +47,8 @@
           <div class="row">  
             <div class="col-12 text-center">
             <label><b>Via</b></label>
-<<<<<<< HEAD
             <input onfocusout="checkStreet('modalStreet','errModalStreet');" onfocusin="cleanErr('errModalStreet')" id="modalStreet" type="textModal" name="street" minlength=<?php echo StreetMinLength?> maxlength=<?php echo StreetMaxLength?> required> 
             <p id="errModalStreet"></p>
-=======
-            <input onfocusout="" onfocusin="" id="modalStreet" type="textModal" name="street" minlength=<?php echo StreetMinLength?> maxlength=<?php echo StreetMaxLength?> required> 
-            <p id=""></p>
->>>>>>> 1c69e0bb514b87200361309ab8652859ae77c9ef
             </div>
           </div>
         </div>
