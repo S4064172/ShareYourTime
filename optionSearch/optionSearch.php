c429a0809fe90ce2696f50d5735304015a2d09bb
Kennedy12 <kennedy12@a725k>
2018-08-28 22:46:49 +0200

    Continuata la validazione dei file

--- optionSearch/optionSearch.php
+++ optionSearch/optionSearch.php
@@ -2,9 +2,10 @@
 <section class="optionSearch" onClick="hideItem('menu');">
 <?php } else{?>
     <section class="optionSearch">
-    <?php }?>           
-        <div class="myContainer" id="resetOption">
-            <div class="row" id="resetOption">
+	<?php }?>      
+        <div class="myContainer text-center pt-5" id="resetOption">
+			<h1><b class="colorTitle">Ricerca</b></h1>	 
+            <div class="row">
                 <div class="col-md-3"> 
                     <input id="optionStreet" onchange="cleanErr('errOptionStreet');checkStreetSearch('optionStreet','errOptionStreet');" class="mySelection" placeholder="Inserisci la via" name="street" minlength=<?php echo StreetMinLength?> maxlength=<?php echo StreetMaxLength?>>
                     <p id="errOptionStreet"></p>
@@ -28,11 +29,11 @@
                         <option selected disabled>Seleziona il costo</option>
                         <?php
                             for($i = 1; $i < 10; $i++)
-                                echo '<option value='.'"'.($i*5).'">'.($i*5).' Euro</option>';
+                                echo '<option value='.'"'.($i*5).'">&lt; '.($i*5).' Euro</option>';
                             for($i = 1; $i < 10; $i++)
-                                echo '<option value='.'"'.($i*50).'">'.($i*50).' Euro</option>';
+                                echo '<option value='.'"'.($i*50).'">&lt; '.($i*50).' Euro</option>';
                             for($i = 1; $i < 10; $i++)
-                                echo '<option value='.'"'.($i*500).'">'.($i*500).' Euro</option>';
+                                echo '<option value='.'"'.($i*500).'">&lt; '.($i*500).' Euro</option>';
                         ?>
                     </select>
                     <p id="errOptionCost"></p>
@@ -48,7 +49,7 @@
                             $conn = connectionToDb();
 
                             if ( !($res = mysqli_query($conn, $getTagsQuery)) ) 
-                                die('Errore nella selezione dei lavori');
+                                die('Errore nella selezione dei tags');
 
                             while( $row = mysqli_fetch_array($res) ) 
                                 echo '<option>'.sanitizeToHtml($row['Tag']).'</option>';
@@ -62,7 +63,7 @@
             </div>
 <?php if( $_SESSION['page'] == 'searchjobs' ) { ?>
             <div class ="row">
-                <div class="offset-md-3 col-md-3"> 
+                <div class="offset-md-3 col-md-6"> 
                     <select class="custom-select mySelection"  id="optionUser" name="userName" onchange="cleanErr('errOptionUser');checkUserName('optionUser','errOptionUser');">
                         <option selected disabled>Seleziona l'utente</option>
                         <?php
@@ -72,13 +73,13 @@
                             $conn = connectionToDb();
 
                             if ( !($res = mysqli_query($conn, $getUserQuery)) ) 
-                                die('Errore nella selezione dei lavori');
+                                die('Errore nella selezione degli utenti');
 
                             while( $row = mysqli_fetch_array($res) ){
                                 $getValuationQuery = "SELECT AVG(Evaluation) FROM ShareYourJobsTime WHERE Proposer ='".$row['User']."' AND Evaluation <> 0;";
 
                                 if ( !($res1 = mysqli_query($conn, $getValuationQuery)) ) 
-                                    die('Errore nella selezione dei lavori');
+                                    die('Errore nella selezione della media di un utente');
                                 $row1 = mysqli_fetch_array($res1);
                                 if ($row['User'] != $_SESSION['user']){
                                     echo '<option>'.sanitizeToHtml($row['User']);
