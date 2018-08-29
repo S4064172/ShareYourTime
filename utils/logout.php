7f36715dea3d7fa30bba223d8cc03707282bc917
Eugenio <s4064172@studenti.unige.it>
2018-08-29 18:00:36 +0200

    -> index.php      -> corretto problema cookie
    -> optionSearch.php
         -> correto refuso

--- utils/logout.php
+++ utils/logout.php
@@ -1,7 +1,10 @@
 <?php
-    session_start();
+    if (session_status() == PHP_SESSION_NONE) {
+        session_start();
+    }   
 
     session_unset();
     session_destroy();
 
-	header("Location:  ../index/index.php");
+
+    header("Location:  ../index/index.php");
