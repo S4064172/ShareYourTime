585bd37baa5cd4d03d3a5cbadac8c785873c2168
Kennedy12 <kennedy12@a725k>
2018-09-03 17:53:09 +0200

    Modificato seed_table

--- db/seed_table.php
+++ db/seed_table.php
@@ -61,6 +61,8 @@
 	insertInto_ShareYourJobsTime("Insegno a fare gli aquiloni", 10, $time3S, $time3E, 2, "DEFAULT", "Corso Alessandro de Stefanis, Genoa, Metropolitan City of Genoa, Italy", 44.4167606, 8.952091800000062, "pippo", "FAI DA TE");
 	insertInto_ShareYourJobsTime("Taglio il prato e gli alberi che ostacolano la strada", 50, $time4S, $time4E, 10, "DEFAULT", "Via Giovanni Amarena, Genoa, Metropolitan City of Genoa, Italy", 44.41349769999999, 8.95928189999995, "pippo", "GIARDINAGGIO");
 	insertInto_ShareYourJobsTime("Preparo pranzo a domicilio", 15, $time5S, $time5E, 1, "DEFAULT", "Via San Martino, Genova, Metropolitan City of Genoa, Italy", 44.3986179, 8.94487730000003, "kenny", "CUCINA");
+	updataInto_ShareYourJobsTime('Receiver', 'br3nd3r', 5);
+
 	insertInto_ShareYourJobsTime("Lavo le scale negli appartamenti", 60, $time6S, $time6E, 2, "DEFAULT", "Via XX Settembre, Genoa, Metropolitan City of Genoa, Italy", 44.4059247, 8.939580200000023, "pippo", "LAVAGGIO");
 
 	insertInto_ShareYourJobsTime("Lavo la macchina nei garage in giro per Genova", 40, $time7S, $time7E, 2, "DEFAULT", "Via San Martino, Genova, Metropolitan City of Genoa, Italy", 44.4058381, 8.970980999999938, "pippo", "LAVAGGIO");
@@ -71,7 +73,7 @@
 	insertInto_ShareYourJobsTime("Design di siti web e consulenza", 100, $time7S, $time7E, 2, "DEFAULT", "Piazza Dante, Genoa, Metropolitan City of Genoa, Italy", 44.4052777, 8.935792699999979, "kenny", "INFORMATICA");
 	
 	insertInto_ShareYourJobsTime("Incontro di patchwork", 5, $time7S, $time7E, 2, "DEFAULT", "Corso Torino, Genoa, Metropolitan City of Genoa, Italy", 44.4013028, 8.950326099999984, "kenny", "FAI DA TE");
-	updataInto_ShareYourJobsTime('Receiver', 'pippo', 10);
+	updataInto_ShareYourJobsTime('Receiver', 'br3nd3r', 10);
 	updataInto_ShareYourJobsTime('Evaluation', 4, 10);
 
 	insertInto_ShareYourJobsTime("Riparo i motorini e le biciclette", 85, $time7S, $time7E, 2, "DEFAULT", "Via Casaregis, Genoa, Metropolitan City of Genoa, Italy", 44.4048195, 8.952552800000035, "pippo", "RIPARAZIONI");
